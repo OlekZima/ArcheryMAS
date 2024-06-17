e83b4391329f22304b74e0691e018dbc4847eac1
OlekZima <zimenkoalex34@gmail.com>
2024-06-17 13:19:58 +0200

    Selecting archers is working!

--- ArcheryMAS/Service/PersonService.cs
+++ ArcheryMAS/Service/PersonService.cs
@@ -1,3 +1,4 @@
+using ArcheryMAS.Components.Pages;
 using ArcheryMAS.Context;
 using ArcheryMAS.Enitites;
 using ArcheryMAS.Migrations;
@@ -16,13 +17,45 @@ public class PersonService
 
     public async Task AddPersonAsync(Person? person)
     {
-        _dbContext.People.Add(person);
+        if (person != null) _dbContext.People.Add(person);
         await _dbContext.SaveChangesAsync();
     }
 
 
-    public async Task<Person?> GetPeopleAsync()
+    public async Task<List<Person>> GetPeopleArchersAsync()
     {
-        return await _dbContext.People.FirstOrDefaultAsync();
+        return await _dbContext.People
+            .Include(p => p.Archer)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<List<Person>> GetPeopleArchersAsync(AddArcherssCompitions.SearchCriteria searchCriteria)
+    {
+        if (_dbContext == null)
+            throw new InvalidOperationException("Database context is not initialized.");
+
+        IQueryable<Person> query = _dbContext.People
+            .Include(p => p.Archer)
+            .AsNoTracking();
+
+        string query2 = "Select * from Person, Archer WHERE Person.PESEL == Archer.Person_PESEL;";
+        var executeSqlRaw = _dbContext.Database.ExecuteSqlRaw(query2);
+
+        Console.WriteLine($"Searching for names containing: {searchCriteria.Names}");
+        Console.WriteLine(query.ToListAsync().Result.Count);
+
+        if (!string.IsNullOrWhiteSpace(searchCriteria.Names))
+            query = query.Where(p => p.Names.Contains(searchCriteria.Names));
+
+        Console.WriteLine(query.ToListAsync().Result.Count);
+
+        if (!string.IsNullOrWhiteSpace(searchCriteria.Surname))
+            query = query.Where(p => p.Surname.Contains(searchCriteria.Surname));
+
+        if (!string.IsNullOrWhiteSpace(searchCriteria.PESEL))
+            query = query.Where(p => p.PESEL.Contains(searchCriteria.PESEL));
+
+        return await query.ToListAsync();
     }
 }
--- ArcheryMAS/ViewModels/ArcherViewModel.cs
+++ ArcheryMAS/ViewModels/ArcherViewModel.cs
@@ -2,8 +2,24 @@ using ArcheryMAS.Enitites;
 
 namespace ArcheryMAS.ViewModels;
 
-public class ArcherViewModel
+public class ArcherPersonViewModel
 {
     public Archer Archer { get; set; }
-    public bool IsSelected { get; set; }
+    public Person Person { get; set; }
+    private bool isSelected;
+
+    public bool IsSelected
+    {
+        get => isSelected;
+        set
+        {
+            if (isSelected != value)
+            {
+                isSelected = value;
+                OnSelectionChanged?.Invoke();
+            }
+        }
+    }
+
+    public Action OnSelectionChanged { get; set; }
 }
